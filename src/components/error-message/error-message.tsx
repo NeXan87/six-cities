94d31504278aece5554c65993ad53d822c761379
NeXan <nexan87@gmail.com>
2023-08-25 10:01:46 +0300

    Изменяет тип вывода в компоненте ErrorMessage

--- src/components/error-message/error-message.tsx
+++ src/components/error-message/error-message.tsx
@@ -2,10 +2,10 @@ import { useAppSelector } from '../../hooks/hooks';
 import { selectErrorMessage } from '../../store/app-process/selectors';
 import styles from './error-message.module.css';
 
-function ErrorMessage(): JSX.Element | boolean {
+function ErrorMessage(): JSX.Element | null {
   const error = useAppSelector(selectErrorMessage);
 
-  return !!error && <div className={styles.error}>{error}</div>;
+  return error ? <div className={styles.error}>{error}</div> : null;
 }
 
 export default ErrorMessage;
